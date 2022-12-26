ba34cd9c3b7935b2275686c366e9e4e18a1f55b9
MURLI PATEL <100035961+murlipatel1@users.noreply.github.com>
2022-12-26 15:14:17 +0530

    Add files via upload

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { AcademicDetails } from '../components/Profile Page Component/AcademicDetails';
+import { Projects } from '../components/Profile Page Component/Projects';
+import { About } from '../components/Profile Page Component/About';
+import { Skills } from '../components/Profile Page Component/Skills';
+import { Clubs } from '../components/Profile Page Component/Clubs';
+import { SocialMedia } from '../components/Profile Page Component/SocialMedia';
+import { Profile } from '../components/Profile Page Component/Profile';
+import { Navbar } from '../components/Profile Page Component/Navbar';
+import { Footer } from '../components/Profile Page Component/Footer';
+
+
+export const ProfilePage = () => {
+  return (
+    <div>
+        <Navbar/>
+      <Profile />
+      <AcademicDetails />
+      <About />
+      <Skills />
+      <Clubs />
+      <SocialMedia />
+      <Projects />
+      <Footer/>
+    </div>
+  )
+}
